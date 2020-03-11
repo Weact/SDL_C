e57a7a016cf4ddf0862874ca6bce4767e102e45c
Weaact <lucas.druckes@numericable.fr>
2020-03-11 10:13:25 +0100

    Question 9 SDL_Renderer, usage and explanation

--- WhatIsThis/main.c
+++ WhatIsThis/main.c
@@ -1,13 +1,11 @@
 #include "../SDL2/SDL.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main(int argc, char *argv[])
 {
 	SDL_Window* window; //Window declaration
-	SDL_Renderer* renderer; //Renderer declaration
-    SDL_Surface *s;
+	SDL_Renderer* renderer; //NEW LINE ! => This is how we declare the renderer.
 
 	if(SDL_Init(SDL_INIT_VIDEO) < 0) // SDL init
 	{
@@ -16,6 +14,11 @@ int main(int argc, char *argv[])
 	}
 
 	window = SDL_CreateWindow("A SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_RESIZABLE);
+	//NEW LINE ! => Here, we create a new window using the following parameters:
+	//Title, X position, Y position, Width, Height, Flag.
+	// SDL_WINDOWPOS_CENTERED as X and Y position will make the window appearing at the center of the screen.
+	// 1280 x 720 will be the size of our window.
+	// SDL_WIDNDOW_RESIZABLE will allow the user to resize the window as he wishes.
 
 	if(window == NULL) //errors management
 	{
@@ -25,6 +28,9 @@ int main(int argc, char *argv[])
 
 	//RENDERER CREATION
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	//NEW LINE! => We create the Renderer of the Window using 3 parameters :
+	//Window where rendering is displayed
+	//SDL_RENDERER_ACCELERATED to use hardware acceleration
 
 	if(renderer == NULL) //errors management
 	{
@@ -32,18 +38,26 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	//Red color
-   	SDL_SetRenderDrawColor(renderer, 100,255,0,255);
+	//To test the renderer, we will want to display something in our window.
+	//First, Select a color by using the following function :
+	SDL_SetRenderDrawColor(renderer, 255,0,255,255);
+			//PARAMETERS: renderer, red, green, blue, alpha (0>255)
 
-    SDL_RenderClear(renderer);
+	//Then clear the window by using SDL_RenderClear function
+	SDL_RenderClear(renderer);
 
-    s = SDL_CreateRGBSurface(0, 200, 200, 32, 0, 0, 0, 0);
-    SDL_FillRect(s, NULL, SDL_MapRGB(s->format, 255, 0, 0));
-
-	DrawCircle(renderer, 150, 300, 150);
-    DrawFilledCircle(renderer, 500, 300, 150);
 	//We have now to display our render using SDL_RenderPresent function
 	SDL_RenderPresent(renderer);
+
+	//We can also create a renderer and a window at the same time this way :
+	/*
+			// PARAMETERS : width, height, flag, &window, &renderer
+	if(SDL_CreateWindowAndRenderer(1280,720, SDL_WINDOW_RESIZABLE,&window,&renderer) < 0)
+	{
+		printf("Error creating renderer and window : %s", SDL_GetError());
+		return EXIT_FAILURE;
+	}
+	*/
 
 	SDL_Delay(3000); //3s break, so the window opens and doesn't close instantly. This method will be explained later.
 
@@ -51,46 +65,4 @@ int main(int argc, char *argv[])
 	SDL_DestroyWindow(window); //Destroy the window
 	SDL_Quit();
 	return 0;
-
-	//This will display a red window, wait 3s and close.
-}
-
-// Dessine le cercle donné
-void DrawCircle(SDL_Renderer *p_renderer, int origin_x, int origin_y, int radius, SDL_Color color)
-{
-    int new_x = 0;
-    int new_y = 0;
-    int old_x =  origin_x + radius;
-    int old_y = origin_y;
-    float step = (M_PI * 2) / 50;
-
-    SDL_SetRenderDrawColor(p_renderer, color.r, color.g, color.b, 255);
-
-    for(float theta = 0; theta <= (M_PI * 2); theta += step){
-        new_x = origin_x + (radius * cos(theta));
-        new_y = origin_y - (radius * sin(theta));
-
-        SDL_RenderDrawLine(p_renderer, old_x, old_y, new_x, new_y);
-
-        old_x = new_x;
-        old_y = new_y;
-    }
-
-    new_x = origin_x + radius;
-    new_y = origin_y;
-    SDL_RenderDrawLine(p_renderer, old_x, old_y, new_x, new_y);
-
-}
-// Dessine le cercle donné, rempli
-void DrawFilledCircle(SDL_Renderer *p_renderer, int origin_x, int origin_y, int radius, SDL_Color color)
-{
-    for(double dy = 1; dy <= radius; dy += 1.0){
-
-        double dx = floor(sqrt((2.0 * radius * dy) - (dy * dy)));
-
-        SDL_SetRenderDrawColor(p_renderer, color.r, color.g, color.b, color.a);
-        SDL_RenderDrawLine(p_renderer, origin_x - dx, origin_y + dy - radius, origin_x + dx, origin_y + dy - radius);
-        SDL_RenderDrawLine(p_renderer, origin_x - dx, origin_y - dy + radius, origin_x + dx, origin_y - dy + radius);
-
-    }
 }
