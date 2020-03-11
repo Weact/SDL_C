2701927d6db82b601e0c93c96bbcfeef5ba50ac4
Weaact <lucas.druckes@numericable.fr>
2020-03-11 10:10:55 +0100

    Question 7, SDL Window

--- WhatIsThis/main.c
+++ WhatIsThis/main.c
@@ -1,13 +1,10 @@
 #include "../SDL2/SDL.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main(int argc, char *argv[])
 {
 	SDL_Window* window; //Window declaration
-	SDL_Renderer* renderer; //Renderer declaration
-    SDL_Surface *s;
 
 	if(SDL_Init(SDL_INIT_VIDEO) < 0) // SDL init
 	{
@@ -16,6 +13,7 @@ int main(int argc, char *argv[])
 	}
 
 	window = SDL_CreateWindow("A SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_RESIZABLE);
+	// resizable 1280x720 centered window creation
 
 	if(window == NULL) //errors management
 	{
@@ -23,74 +21,9 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	//RENDERER CREATION
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_Delay(1000); //1s break, so the window opens and doesn't close instantly. This method will be explained later.
 
-	if(renderer == NULL) //errors management
-	{
-		printf("Error creating the renderer : %s", SDL_GetError());
-		return EXIT_FAILURE;
-	}
-
-	//Red color
-   	SDL_SetRenderDrawColor(renderer, 100,255,0,255);
-
-    SDL_RenderClear(renderer);
-
-    s = SDL_CreateRGBSurface(0, 200, 200, 32, 0, 0, 0, 0);
-    SDL_FillRect(s, NULL, SDL_MapRGB(s->format, 255, 0, 0));
-
-	DrawCircle(renderer, 150, 300, 150);
-    DrawFilledCircle(renderer, 500, 300, 150);
-	//We have now to display our render using SDL_RenderPresent function
-	SDL_RenderPresent(renderer);
-
-	SDL_Delay(3000); //3s break, so the window opens and doesn't close instantly. This method will be explained later.
-
-	SDL_DestroyRenderer(renderer); //Destroy the renderer
-	SDL_DestroyWindow(window); //Destroy the window
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
-
-	//This will display a red window, wait 3s and close.
-}
-
-// Dessine le cercle donné
-void DrawCircle(SDL_Renderer *p_renderer, int origin_x, int origin_y, int radius, SDL_Color color)
-{
-    int new_x = 0;
-    int new_y = 0;
-    int old_x =  origin_x + radius;
-    int old_y = origin_y;
-    float step = (M_PI * 2) / 50;
-
-    SDL_SetRenderDrawColor(p_renderer, color.r, color.g, color.b, 255);
-
-    for(float theta = 0; theta <= (M_PI * 2); theta += step){
-        new_x = origin_x + (radius * cos(theta));
-        new_y = origin_y - (radius * sin(theta));
-
-        SDL_RenderDrawLine(p_renderer, old_x, old_y, new_x, new_y);
-
-        old_x = new_x;
-        old_y = new_y;
-    }
-
-    new_x = origin_x + radius;
-    new_y = origin_y;
-    SDL_RenderDrawLine(p_renderer, old_x, old_y, new_x, new_y);
-
-}
-// Dessine le cercle donné, rempli
-void DrawFilledCircle(SDL_Renderer *p_renderer, int origin_x, int origin_y, int radius, SDL_Color color)
-{
-    for(double dy = 1; dy <= radius; dy += 1.0){
-
-        double dx = floor(sqrt((2.0 * radius * dy) - (dy * dy)));
-
-        SDL_SetRenderDrawColor(p_renderer, color.r, color.g, color.b, color.a);
-        SDL_RenderDrawLine(p_renderer, origin_x - dx, origin_y + dy - radius, origin_x + dx, origin_y + dy - radius);
-        SDL_RenderDrawLine(p_renderer, origin_x - dx, origin_y - dy + radius, origin_x + dx, origin_y - dy + radius);
-
-    }
 }
