29b20523caa093634e18283ac4b09ab24c26b576
Weaact <lucas.druckes@numericable.fr>
2020-03-11 10:29:32 +0100

    Q12 Red background

--- WhatIsThis/main.c
+++ WhatIsThis/main.c
@@ -7,7 +7,6 @@ int main(int argc, char *argv[])
 {
 	SDL_Window* window; //Window declaration
 	SDL_Renderer* renderer; //Renderer declaration
-    SDL_Surface *s;
 
 	if(SDL_Init(SDL_INIT_VIDEO) < 0) // SDL init
 	{
@@ -32,16 +31,10 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	//Red color
-   	SDL_SetRenderDrawColor(renderer, 100,255,0,255);
-
-    SDL_RenderClear(renderer);
-
-    s = SDL_CreateRGBSurface(0, 200, 200, 32, 0, 0, 0, 0);
-    SDL_FillRect(s, NULL, SDL_MapRGB(s->format, 255, 0, 0));
-
-	DrawCircle(renderer, 150, 300, 150);
-    DrawFilledCircle(renderer, 500, 300, 150);
+//Red color
+	SDL_SetRenderDrawColor(renderer, 255,0,0,255);
+	//Then clear the window by using SDL_RenderClear function
+	SDL_RenderClear(renderer);
 	//We have now to display our render using SDL_RenderPresent function
 	SDL_RenderPresent(renderer);
 
@@ -53,44 +46,4 @@ int main(int argc, char *argv[])
 	return 0;
 
 	//This will display a red window, wait 3s and close.
-}
-
-// Dessine le cercle donné
-void DrawCircle(SDL_Renderer *p_renderer, int origin_x, int origin_y, int radius, SDL_Color color)
-{
-    int new_x = 0;
-    int new_y = 0;
-    int old_x =  origin_x + radius;
-    int old_y = origin_y;
-    float step = (M_PI * 2) / 50;
-
-    SDL_SetRenderDrawColor(p_renderer, color.r, color.g, color.b, 255);
-
-    for(float theta = 0; theta <= (M_PI * 2); theta += step){
-        new_x = origin_x + (radius * cos(theta));
-        new_y = origin_y - (radius * sin(theta));
-
-        SDL_RenderDrawLine(p_renderer, old_x, old_y, new_x, new_y);
-
-        old_x = new_x;
-        old_y = new_y;
-    }
-
-    new_x = origin_x + radius;
-    new_y = origin_y;
-    SDL_RenderDrawLine(p_renderer, old_x, old_y, new_x, new_y);
-
-}
-// Dessine le cercle donné, rempli
-void DrawFilledCircle(SDL_Renderer *p_renderer, int origin_x, int origin_y, int radius, SDL_Color color)
-{
-    for(double dy = 1; dy <= radius; dy += 1.0){
-
-        double dx = floor(sqrt((2.0 * radius * dy) - (dy * dy)));
-
-        SDL_SetRenderDrawColor(p_renderer, color.r, color.g, color.b, color.a);
-        SDL_RenderDrawLine(p_renderer, origin_x - dx, origin_y + dy - radius, origin_x + dx, origin_y + dy - radius);
-        SDL_RenderDrawLine(p_renderer, origin_x - dx, origin_y - dy + radius, origin_x + dx, origin_y - dy + radius);
-
-    }
 }
